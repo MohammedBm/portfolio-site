6986ba58fda26bb469036c6a9a603646a63a47ce
james-ciclitira <jciclitira@gmail.com>
2018-06-10 09:30:07 +0200

    vulnerability-dependencies

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -6,39 +6,28 @@ exports.components = {
   "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/src/pages/404.js"),
   "component---src-pages-crowd-scores-js": require("gatsby-module-loader?name=component---src-pages-crowd-scores-js!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/src/pages/CrowdScores.js"),
   "component---src-pages-gradient-js": require("gatsby-module-loader?name=component---src-pages-gradient-js!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/src/pages/Gradient.js"),
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/src/pages/index.js"),
   "component---src-pages-numilli-js": require("gatsby-module-loader?name=component---src-pages-numilli-js!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/src/pages/Numilli.js"),
+  "component---src-pages-page-2-js": require("gatsby-module-loader?name=component---src-pages-page-2-js!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/src/pages/page-2.js"),
   "component---src-pages-quidco-js": require("gatsby-module-loader?name=component---src-pages-quidco-js!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/src/pages/Quidco.js"),
   "component---src-pages-suggestv-js": require("gatsby-module-loader?name=component---src-pages-suggestv-js!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/src/pages/Suggestv.js"),
   "component---src-pages-thanks-js": require("gatsby-module-loader?name=component---src-pages-thanks-js!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/src/pages/Thanks.js"),
-  "component---src-pages-yunojuno-js": require("gatsby-module-loader?name=component---src-pages-yunojuno-js!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/src/pages/Yunojuno.js"),
-  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/src/pages/index.js"),
-  "component---src-pages-page-2-js": require("gatsby-module-loader?name=component---src-pages-page-2-js!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/src/pages/page-2.js")
+  "component---src-pages-yunojuno-js": require("gatsby-module-loader?name=component---src-pages-yunojuno-js!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/src/pages/Yunojuno.js")
 }
 
 exports.json = {
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/layout-index.json"),
   "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/dev-404-page.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/layout-index.json"),
   "404.json": require("gatsby-module-loader?name=path---404!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/404.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/layout-index.json"),
   "crowd-scores.json": require("gatsby-module-loader?name=path---crowd-scores!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/crowd-scores.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/layout-index.json"),
   "gradient.json": require("gatsby-module-loader?name=path---gradient!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/gradient.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/layout-index.json"),
-  "numilli.json": require("gatsby-module-loader?name=path---numilli!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/numilli.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/layout-index.json"),
-  "quidco.json": require("gatsby-module-loader?name=path---quidco!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/quidco.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/layout-index.json"),
-  "suggestv.json": require("gatsby-module-loader?name=path---suggestv!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/suggestv.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/layout-index.json"),
-  "thanks.json": require("gatsby-module-loader?name=path---thanks!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/thanks.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/layout-index.json"),
-  "yunojuno.json": require("gatsby-module-loader?name=path---yunojuno!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/yunojuno.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/layout-index.json"),
   "index.json": require("gatsby-module-loader?name=path---index!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/index.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/layout-index.json"),
+  "numilli.json": require("gatsby-module-loader?name=path---numilli!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/numilli.json"),
   "page-2.json": require("gatsby-module-loader?name=path---page-2!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/page-2.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/layout-index.json"),
+  "quidco.json": require("gatsby-module-loader?name=path---quidco!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/quidco.json"),
+  "suggestv.json": require("gatsby-module-loader?name=path---suggestv!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/suggestv.json"),
+  "thanks.json": require("gatsby-module-loader?name=path---thanks!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/thanks.json"),
+  "yunojuno.json": require("gatsby-module-loader?name=path---yunojuno!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/yunojuno.json"),
   "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/404-html.json")
 }
 

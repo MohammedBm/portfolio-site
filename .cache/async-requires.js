b47229408c7c2a08dfb5e71a23a313c35cba3c9a
james-ciclitira <jciclitira@gmail.com>
2018-06-10 15:36:47 +0200

    preview updata

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,6 +2,7 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
+  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/dev-404-page.js"),
   "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/src/pages/404.js"),
   "component---src-pages-crowd-scores-js": require("gatsby-module-loader?name=component---src-pages-crowd-scores-js!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/src/pages/CrowdScores.js"),
   "component---src-pages-gradient-js": require("gatsby-module-loader?name=component---src-pages-gradient-js!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/src/pages/Gradient.js"),
@@ -16,6 +17,7 @@ exports.components = {
 
 exports.json = {
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/dev-404-page.json"),
   "404.json": require("gatsby-module-loader?name=path---404!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/404.json"),
   "crowd-scores.json": require("gatsby-module-loader?name=path---crowd-scores!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/crowd-scores.json"),
   "gradient.json": require("gatsby-module-loader?name=path---gradient!/Users/Jimmy/Documents/jciclitira.com/portfolio-site/.cache/json/gradient.json"),
